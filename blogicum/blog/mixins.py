35be16e896b823f6b4cf3b8080cfa3bcccdbfe2d
Aleksandra Peskova <peskoval88@gmail.com>
2025-06-08 11:36:09 +0300

    added mixins to git

---
+++ blogicum/blog/mixins.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.shortcuts import redirect
+from django.urls import reverse
+
+class AuthorTests(UserPassesTestMixin):
+
+    def test_func(self):
+        return self.request.user == self.get_object().author
+
+    def handle_no_permission(self):
+        return redirect(
+            reverse(
+                'blog:post_detail',
+                kwargs={'post_id': self.get_object().id}
+            ))
